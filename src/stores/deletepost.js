0a2d84fe72ccbe86f7d16d87216e4bd1d9ab0a0d
abdulazizv <abdulazizheir@gmail.com>
2023-03-13 20:12:25 +0500

    added deletepost

--- src/stores/deletepost.js
+++ src/stores/deletepost.js
@@ -1,15 +1,14 @@
 import {defineStore} from 'pinia'
 import axios from '../service/axios'
 
-export const deletePost = defineStore('deletepost', {
+export const deletepost = defineStore('deletepost', {
     state: () => (
         {errMessage: "", status: null}
     ),
     actions: {
         async deletePost(id) {
             try {
-                const response = await axios.delete('/posts',1);
-
+                const response = await axios.delete(`/posts/${id}`);
                 if (response.status === 200) {
                     const data = response.data;
                     this.status = data.status;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,31 @@
 <script setup>
-import { onMounted } from "vue";
-import { count } from "@/stores/counter";
-import { getpost } from "@/stores/getpost";
-import {deletepost} from '@/stores/deletepost';
-const store = count();
+import { onMounted } from 'vue'
+import { count } from '@/stores/counter'
+import { getpost } from '@/stores/getpost'
+import { deletepost } from '@/stores/deletepost'
+const store = count()
 const data = getpost();
-const newArr = data.sendPost;
+const newArr = data.sendPost
 console.log(newArr)
 console.log(data)
+
+const deletePost =deletepost();
+
+
+const deletPost = (id) => {
+    deletePost.deletePost(2);
+}
+
 onMounted(() => {
   data.getPosts()
 })
 </script>
 
-
 <template>
   <h1>{{ store.number }}</h1>
 
-
   <button @click="store.increment">ADD</button>
+  <button @click="deletPost">DELETE</button>
 </template>
-
-
 
 <style></style>
